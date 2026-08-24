527995a92f307b3554d77b8791914d64ad46a9b9
Produtora Bādon <produtorabadon@MacBookPro.home>
2026-08-24 14:48:18 -0300

    Remover Meta Pixel do projeto Florescer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,38 +8,5 @@ const sans = Manrope({ variable: "--font-sans", subsets: ["latin"], weight: ["40
 export const metadata: Metadata = { title: "Florescer na Clínica | Método Jardim", description: "Estruture seu consultório de psicologia com estratégia, ética e espaço para viver." };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="pt-BR">
-      <head>
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '764159417596803');
-fbq('track', 'PageView');`,
-          }}
-        />
-        {/* End Meta Pixel Code */}
-      </head>
-      <body className={`${display.variable} ${sans.variable}`}>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=764159417596803&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-        {children}
-      </body>
-    </html>
-  );
+  return <html lang="pt-BR"><body className={`${display.variable} ${sans.variable}`}>{children}</body></html>;
 }
